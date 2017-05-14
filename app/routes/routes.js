50589ecc0c6ca8f603a26e20e0d9647e40f76928
Phạm Quốc Toàn <bossdiemmaimai@gmail.com>
2017-05-14 08:49:46 -0700

    make it better

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -12,7 +12,7 @@ module.exports = function(app, passport, pool, controllers) {
 
 	// process the login form
 	app.post('/login', passport.authenticate('local-login', {
-            successRedirect : '/profile', // redirect to the secure profile section
+            successRedirect : '/messages/inbox-messages', // redirect to the secure profile section
             failureRedirect : '/login', // redirect back to the signup page if there is an error
             failureFlash : true // allow flash messages
 		}),
